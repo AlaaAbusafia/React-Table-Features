cc8209264921fa4e1b30097053c62e8c9ce9910f
Jason Liao <jason@archipro.co.nz>
2018-10-09 15:09:17 +1300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Container, Row, Col, Table } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
+import data from './api/data.json';
+import logo from './archipro_dev.webp';
 
 class App extends Component {
+
+
+  getTable() {
+    return (
+      <Table className="App-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Contact Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.getRow()}
+        </tbody>
+      </Table>
+    );
+  }
+
+  getRow() {
+    return data.map(
+      ({ _id, name, email, phone }) => (
+        <tr key={_id}>
+          <td>{name}</td>
+          <td>{email}</td>
+          <td>{phone}</td>
+        </tr>
+      )
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <main className="App-content">
+          <Container>
+            <Row>
+              <Col>
+              {this.getTable()}
+              </Col>
+            </Row>
+          </Container>
+
+        </main>
       </div>
     );
   }
