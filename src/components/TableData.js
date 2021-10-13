3ccaa9242e189b630a93e07fae34b232aeb65f67
Alaa <alaa@Ahmeds-Air.lan>
2021-10-14 10:10:19 +1300

    Adding sorting functionality by creating SortingData component

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -1,13 +1,32 @@
 import React from 'react'
-import { Table } from 'reactstrap';
+import { Table , Button} from 'reactstrap';
+import SortingData from './SortingData';
 
 //TableData component to display the data 
 const TableData = (props) => {
+    //add sorting functionality 
+    const {rows, requestSort, sortConfig } = SortingData(props.dataSource);
     
+    //Add the sorting to the column depends on it's className
+    const getClassNamesFor = (name) =>{
+        if(!sortConfig) {
+            return;
+        }
+        return sortConfig.key === name ? sortConfig.direction : undefined;
+    }
+
     return (
          <Table className="App-table" >
             <thead>
-                <th>Name</th>
+                <th>
+                    <Button
+                        outline
+                        color="secondary"
+                        //change click handler
+                        onClick={() => requestSort('name')}
+                        className={getClassNamesFor('name')}
+                    >
+                        <span>Name</span></Button></th>
                 <th>Email</th>
                 <th>Phone</th>
             </thead>
