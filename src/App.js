49c2457d25b985db0fe74589b449e6aefa2699ba
Alaa <alaa@Ahmeds-Air.lan>
2021-10-14 10:51:03 +1300

    Adding Search Filter for only the displayed parameters

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col, Table } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TableData from './components/TableData';
+import SearchFilter from './components/SearchFilter';
 
 import './App.css';
 import data from './api/data.json';
 import logo from './archipro_dev.webp';
 
-class App extends Component {
+const App =() => {
 
+  const [dataSource, setDataSource] = useState(data);
 
-
-
-  render() {
-    return (
+  const filterTable =(filterTable) =>{
+      setDataSource(filterTable)
+  }
+  
+  return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
         <main className="App-content">
           <Container>
+            <SearchFilter dataSource={data}  filterTable={filterTable} />
             <Row>
               <Col>
-              <TableData dataSource={data} />
+              <TableData dataSource={dataSource} />
               </Col>
             </Row>
           </Container>
@@ -30,7 +34,6 @@ class App extends Component {
         </main>
       </div>
     );
-  }
 }
 
 export default App;
